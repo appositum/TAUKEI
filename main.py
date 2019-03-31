3c3a041e683d8fb8aa9b3943fc68fc41a2c0bd7e
ascustodio <alex.scustod@gmail.com>
2019-03-31 17:31:41 -0300

    New commands

--- main.py
+++ main.py
@@ -16,5 +16,8 @@ async def on_message(message):
         return
     if message.content == "oi":
         await client.send_message(message.channel, "Mano namoral o Anemonas me criou então cala a boquita aí por favor. Obrigado")
-
+    elif message.content == "alex":
+        await client.send_message(message.channel, "VOCÊ DISSE ALEX???? AQUELE LINDO??? ``AGR PABLLO VITTAR FOI MUITO LONGE QUANDO DISSE ISSO`` :sunglasses:	PSE ")
+    elif message content == "admin":
+	await client.send_message(message.channel, "RESPEITAR O ADMIN OK?")		
 client.run(token)
